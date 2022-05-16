2e6b04197221a8f1e3425f903aa2f0d06afb42a3
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-17 00:49:33 +0900

    :tada: Start project

---
+++ Egg Timer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  Egg Timer
+//
+//  Created by 홍승현 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
