93305cd08010b5486075a5f8adac5862f4db7267
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-17 01:24:04 +0900

    :lipstick: 색상 열거형 추가
    - 배경색 및 포인트 색 추가

---
+++ Egg Timer/Constants/Color.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+enum Color {
+  
+  /// #FDDFE0 color (light pink)
+  static let appBackgroundColor       = UIColor(red: 0.992, green: 0.875, blue: 0.878, alpha: 1)
+  
+  /// #FFBBBB color (semi dark pink)
+  static let appSubBackgroundColor    = UIColor(red: 1, green: 0.733, blue: 0.733, alpha: 1)
+  
+  /// #FF7B73 color (semi light red)
+  static let appPointColor            = UIColor(red: 1, green: 0.482, blue: 0.451, alpha: 1)
+  
+}
