e5a9b3dcb03a59f15b05507f1c12943a528a6dc1
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-17 22:11:13 +0900

    :construction: 세팅 뷰 컨트롤러 추가

---
+++ Egg Timer/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingsViewController.swift
+//  Egg Timer
+//
+//  Created by 홍승현 on 2022/05/17.
+//
+
+import UIKit
+
+class SettingsViewController: BaseViewController {
+  
+  
+  override func setupLayout() {
+    
+  }
+  
+  override func setupConstraints() {
+    
+  }
+  
+  override func setupStyle() {
+    
+  }
+  
+}
