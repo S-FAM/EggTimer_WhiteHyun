f1f76adf1899e4971d3b2cde2ede8a8953655ca9
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-22 21:41:48 +0900

    :speech_balloon: 상수 값 추가
    - Notification.Name 상수 추가
    - UserDefaults 타이머 키값 추가

---
+++ Egg Timer/Constants/NotificationName.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+extension Notification.Name {
+  
+  static let updateTimerValue = Notification.Name("updateTimerValue")
+  
+}
--- Egg Timer/Constants/UserDefaults.swift
+++ Egg Timer/Constants/UserDefaults.swift
@@ -3,3 +3,10 @@ enum SettingValue {
   static let switchClockKey = "switchClockKey"
   
 }
+
+
+enum TimerTime {
+  
+  static let didEnterBackground = "didEnterBackground"
+  
+}
