d719e846404cb69200fd9341b1e8effae97c36ff
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-05-23 10:55:22 +0900

    :fire: 공백 일부 제거

--- Egg Timer/Source/ViewControllers/Settings/SettingTableViewCell.swift
+++ Egg Timer/Source/ViewControllers/Settings/SettingTableViewCell.swift
@@ -31,7 +31,7 @@ class SettingTableViewCell: UITableViewCell {
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-    
+  
   func setupLayout() {
     contentView.addSubview(label)
     contentView.addSubview(settingSwitch)
